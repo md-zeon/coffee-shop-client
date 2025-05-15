909b1dd5abbb63265eb9418fb4b1a2d878236924
md-zeon <zeonrahaman5870@gmail.com>
2025-05-15 12:32:44 +0600

    Added Delete Functionality with id

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,67 @@
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+	const { _id, name, quantity, price, photo } = coffee;
+
+	const handleDelete = (id) => {
+		console.log("Delete Coffee");
+		Swal.fire({
+			title: "Are you sure?",
+			text: "You won't be able to revert this!",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes, delete it!",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				fetch(`http://localhost:3000/coffees/${id}`, {
+					method: "DELETE",
+				})
+					.then((res) => res.json())
+					.then((data) => {
+						if (data.deletedCount) {
+							Swal.fire({
+								title: "Deleted!",
+								text: "Your Coffee has been deleted.",
+								icon: "success",
+							});
+                            console.log("After Delete", data);
+						}
+					});
+			}
+		});
+	};
+
+	return (
+		<div className='card card-side bg-base-100 shadow-sm border-2'>
+			<figure>
+				<img
+					src={photo}
+					alt={name}
+				/>
+			</figure>
+			<div className='flex justify-around w-full'>
+				<div>
+					<h2>{name}</h2>
+					<p>Price: {price}</p>
+					<p>Quantity: {quantity}</p>
+				</div>
+				<div className='card-actions justify-end items-center'>
+					<div className='join join-vertical'>
+						<button className='btn join-item btn-primary'>View</button>
+						<button className='btn join-item btn-accent'>Edit</button>
+						<button
+							onClick={() => handleDelete(_id)}
+							className='btn join-item btn-error'
+						>
+							Delete
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CoffeeCard;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
 import { useLoaderData } from "react-router";
+import CoffeeCard from "./CoffeeCard";
 
 const Home = () => {
     const coffees = useLoaderData();
     console.log(coffees);
     return (
         <div>
-            Home
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {
+                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+                }
+            </div>
         </div>
     );
 };
